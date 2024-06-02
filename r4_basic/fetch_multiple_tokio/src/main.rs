7ae88e1f8cddf222b9740f8302cb7bc023b9121c
Dot8Pixels <napatsriz@gmail.com>
2024-06-02 22:14:58 +0700

    feat: :sparkles: Fetch multiple with Tokio

---
+++ r4_basic/fetch_multiple_tokio/src/main.rs
@@ -0,0 +1,49 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use tokio::task;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct AnimalData {
+    id: String,
+    weight: f32,
+    created_at: String,
+}
+
+async fn fetch(url: &str) -> anyhow::Result<AnimalData> {
+    Ok(Client::new()
+        .get(url)
+        .send()
+        .await?
+        .json::<AnimalData>()
+        .await?)
+}
+
+async fn fetch_multiple(urls: [&'static str; 2]) -> anyhow::Result<Vec<AnimalData>> {
+    // How to use task::spawn
+    let tasks = urls.iter().map(|url| task::spawn(fetch(url)));
+
+    // Collect task result.
+    let mut results = vec![];
+    for task in tasks {
+        results.push(task.await?);
+    }
+
+    // Return flattened results, silent if error.
+    Ok(results
+        // We use into_iter so we get Vec<AnimalData> instead of Vec<&AnimalData>
+        .into_iter()
+        .flatten()
+        .collect::<Vec<_>>())
+}
+
+#[tokio::main]
+async fn main() {
+    let urls:[&'static str;2] = [
+        "https://raw.githubusercontent.com/gist-rs/book/main/examples/r4/31-fetch-multiple-tokio/src/foo.json",
+        "https://raw.githubusercontent.com/gist-rs/book/main/examples/r4/31-fetch-multiple-tokio/src/bar.json" 
+    ];
+
+    let json = fetch_multiple(urls).await;
+    println!("{json:#?}");
+}
